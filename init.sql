ca7ed5d3689d4d67ae2542eacdd9c36eab766a91
Hardwaregore <hardwaregore101@gmail.com>
2022-07-08 14:24:43 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,3 +1,4 @@
+
 CREATE USER 'mgr'@'localhost' IDENTIFIED BY 'MGRpasswd1234';
 
 GRANT INSERT, CREATE, UPDATE, DELETE, SELECT on *.* TO 'mgr'@'localhost' WITH GRANT OPTION;
@@ -9,6 +10,13 @@ USE `MGR`
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCRIMENT PRIMARY KEY,
   `username` varchar(15) NOT NULL,
-  `passwd` varchar(30)
+  `mstrpasswd` varchar(30)
+  
+);
+
+CREATE TABLE `accounts` (
+  `id` int NOT NULL AUTO_INCRIMENT PRIMARY KEY,
+  `username/hostname` varchar(15) NOT NULL,
+  `password` varchar(30)
   
 );
