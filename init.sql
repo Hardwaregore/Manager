e12edf40a0185879745e8e43e04238b4ceab497f
Hardwaregore <hardwaregore101@gmail.com>
2022-07-08 14:22:38 -0500

    Update init.sql

--- init.sql
+++ init.sql
@@ -9,13 +9,6 @@ USE `MGR`
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCRIMENT PRIMARY KEY,
   `username` varchar(15) NOT NULL,
-  `mstrpasswd` varchar(30)
-  
-);
-
-CREATE TABLE `root_passwds` (
-  `id` int NOT NULL AUTO_INCRIMENT PRIMARY KEY,
-  `username/hostname` varchar(15) NOT NULL,
-  `passord` varchar(30)
+  `passwd` varchar(30)
   
 );
